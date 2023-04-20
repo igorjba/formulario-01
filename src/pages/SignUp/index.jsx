6c033e59efa77b38f22c654e3f8d5d35f6624061
Igor Jardim Bahia <igorjbahia@gmai.com>
2023-04-20 09:02:23 -0300

    :sparkles: feat: Formulário criado.

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,12 +1,60 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import backgroundImage from '../../assets/background.jpg'
 import './style.css'
 
 function SignUp() {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+  const [error, setError] = useState('');
+
+  function handleChangeForm(event) {
+
+  }
+
   return (
     <div className="container">
-      <div></div>
+      <div className='left'>
+        <h1>Cadastre-se</h1>
+        <form>
+          <input
+            name='name'
+            type="text"
+            placeholder='Name'
+            value={form.name}
+            onChange={(event) => handleChangeForm(event)}
+          />
+          <input
+            name='email'
+            type="text"
+            placeholder='Email'
+            value={form.email}
+            onChange={(event) => handleChangeForm(event)}
+          />
+          <input
+            name='password'
+            type="text"
+            placeholder='Password'
+            value={form.password}
+            onChange={(event) => handleChangeForm(event)}
+          />
+          <div className='container-btn'>
+            <button type='submit' className=''>Cadastrar</button>
+            <button type='button' className=''>Cancelar</button>
+            {error && <span className='error-message'>{error}</span>}
+          </div>
+        </form>
+        <div className='container-link'>
+          <span>Já tem cadastro?</span>
+          <a href="#">Clique aqui</a>
+        </div>
+      </div>
+      <img
+        className='right'
+        src={backgroundImage}
+        alt="background" />
 
     </div>
   )
