504afb5e2b74742c8b132edc6dcb599c676316ad
Igor Jardim Bahia <igorjbahia@gmai.com>
2023-04-20 08:37:39 -0300

    :sparkles: feat: Realizada a organização dos arquivos.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import SignUp from './pages/SignUp'
+import './style/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SignUp />
   </React.StrictMode>,
 )
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './style.css'
+
+function SignUp() {
+  return (
+    <div className="container">
+      <div></div>
+
+    </div>
+  )
+}
+
+export default SignUp
